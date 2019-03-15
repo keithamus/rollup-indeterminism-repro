0d4b36dfa5b0e98d122a33fa80175ee7453e36fa
Keith Cirkel <keithamus@users.noreply.github.com>
2019-03-15 19:01:00 +0000

    chore: rejig output dots

--- script/run.sh
+++ script/run.sh
@@ -6,11 +6,12 @@ rm -rf dist/
 mkdir -p dist/
 while true; do
   rollup -c rollup.conf.js &>dist/rollup.log
-  printf '.'
   count=$(ls dist/*.js | wc -l)
   if [ $count -gt 1 ]; then
-    printf '\n'
+    printf 'F\n'
     ls -t dist/*.js | xargs diff -U1
     exit 1
+  else
+    printf '.'
   fi
 done
