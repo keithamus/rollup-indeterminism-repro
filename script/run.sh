7aece9abe28b72a153677a860c205b950e99a46e
Keith Cirkel <keithamus@users.noreply.github.com>
2019-03-15 19:04:53 +0000

    chore: add run count

--- script/run.sh
+++ script/run.sh
@@ -4,12 +4,15 @@ trap 'cat dist/rollup.log >&2' ERR
 
 rm -rf dist/
 mkdir -p dist/
+run=0
 while true; do
-  rollup -c rollup.conf.js &>dist/rollup.log
-  count=$(ls dist/*.js | wc -l)
+  run=$((run+1))
+  RUN=${run} ./node_modules/.bin/rollup -c rollup.conf.js &>dist/rollup.log
+  count=$(ls dist/index-*.js | wc -l)
   if [ $count -gt 1 ]; then
     printf 'F\n'
     ls -t dist/*.js | xargs diff -U1
+    echo "After $run runs"
     exit 1
   else
     printf '.'
